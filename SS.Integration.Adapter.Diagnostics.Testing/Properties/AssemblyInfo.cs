820a07023df6287e58a592079c5378f9a307f58e
Jedrzej Sztompka <jsztompka@sportingindex.com>
2014-11-28 09:38:47 +0000

    Adding license to the files

--- SS.Integration.Adapter.Diagnostics.Testing/Properties/AssemblyInfo.cs
+++ SS.Integration.Adapter.Diagnostics.Testing/Properties/AssemblyInfo.cs
@@ -1,4 +1,18 @@
-﻿using System.Reflection;
+//Copyright 2014 Spin Services Limited
+
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -9,7 +23,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SS.Integration.Adapter.Diagnostics.Testing")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright �  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,3 +47,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
--- SS.Integration.Adapter.Diagnostics/Supervisor.cs
+++ SS.Integration.Adapter.Diagnostics/Supervisor.cs
@@ -1,4 +1,18 @@
-﻿using System;
+//Copyright 2014 Spin Services Limited
+
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
@@ -357,7 +371,7 @@ namespace SS.Integration.Adapter.Diagnostics
 
         public virtual bool RemoveStreamListener(string fixtureId)
         {
-            base.RemoveStreamListener(fixtureId);
+            var result = base.RemoveStreamListener(fixtureId);
 
             var fixtureState = EventState.GetFixtureState(fixtureId);
             if(fixtureState != null && fixtureState.MatchStatus == MatchStatus.MatchOver)
@@ -365,6 +379,9 @@ namespace SS.Integration.Adapter.Diagnostics
                 FixtureOverview tempObj = null;
                 _fixtures.TryRemove(fixtureId, out tempObj);
             }
+
+            return result;
         }
     }
 }
+
