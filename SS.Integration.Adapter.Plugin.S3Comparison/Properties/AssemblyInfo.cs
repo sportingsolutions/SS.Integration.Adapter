b027b57cd788d001271c6088ff2f69b42bd5d1a1
Dave Sansum <dsansum@sportingsolutions.com>
2016-05-12 12:23:32 +0100

    Adding in S3 connector plugin and setup

---
+++ SS.Integration.Adapter.Plugin.S3Comparison/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿//Copyright 2014 Spin Services Limited
+
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SS.Integration.Adapter.Plugin.S3Comparison")]
+[assembly: AssemblyDescription("Default Sporting Solution Integration Adapter's plug-in")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("SS.Integration.Adapter")]
+[assembly: AssemblyCompany("Sporting Solutions")]
+[assembly: AssemblyCopyright("Copyright (c) Sporting Solutions 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0.0")]
+
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6738e8ee-2683-4f90-bc22-73be3bb01f2b")]
---
+++ SS.Integration.Adapter.Plugin.S3Comparison/S3Connector.cs
@@ -0,0 +1,107 @@
+﻿//Copyright 2014 Spin Services Limited
+
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Configuration;
+using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Newtonsoft.Json;
+using SS.Integration.Adapter.Model;
+using SS.Integration.Adapter.Model.Interfaces;
+
+
+namespace SS.Integration.Adapter.Plugin.S3Comparison
+{
+    [Export(typeof(IAdapterPlugin))]
+    public class LoggerConnector : IAdapterPlugin
+    {
+        private static readonly string AwsAccessKey = ConfigurationManager.AppSettings["Comparison.AWSAccessKey"];
+        private static readonly string AwsSecretKey = ConfigurationManager.AppSettings["Comparison.AWSSecretKey"];
+        private static readonly string AwsBucket = ConfigurationManager.AppSettings["Comparison.AWSBucket"];
+        private static readonly string ServiceEnv = ConfigurationManager.AppSettings["Comparison.ServiceEnvironment"];
+        private static readonly string ServiceMode = ConfigurationManager.AppSettings["Comparison.ServiceMode"];
+
+        public void Initialise()
+        {
+
+        }
+
+        public void ProcessSnapshot(Fixture fixture, bool hasEpochChanged = false)
+        {
+            WriteToS3(fixture, true);
+        }
+
+        public void ProcessStreamUpdate(Fixture fixture, bool hasEpochChanged = false)
+        {
+            WriteToS3(fixture);
+        }
+
+        public void ProcessMatchStatus(Fixture fixture)
+        {
+
+        }
+
+        public void ProcessFixtureDeletion(Fixture fixture)
+        {
+
+        }
+
+        public void UnSuspend(Fixture fixture)
+        {
+
+        }
+
+        public void Suspend(string fixtureId)
+        {
+
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        public IEnumerable<IMarketRule> MarketRules => new List<IMarketRule>();
+
+        private static void WriteToS3(Fixture fixture, bool isSnapshot = false)
+        {
+            using (var client = new AmazonS3Client(
+                new BasicAWSCredentials(AwsAccessKey, AwsSecretKey),
+                Amazon.RegionEndpoint.EUWest1))
+            {
+                var now = DateTime.UtcNow;
+                var date = now.ToString("yyyyMMdd");
+                var time = now.ToString("HHmmss");
+                var objectType = isSnapshot ? "snapshot" : "delta";
+                
+                var fixtureNormalized = fixture.FixtureName.Replace(" ", string.Empty);
+
+                var key = $"adapter-comparisons/{ServiceEnv}/{date}/{fixtureNormalized}/{ServiceMode}/{fixture.Sequence}-{objectType}-{time}.json";
+
+                var request = new PutObjectRequest
+                {
+                    BucketName = AwsBucket,
+                    Key = key,
+                    ContentBody = JsonConvert.SerializeObject(fixture, Formatting.Indented),
+                    ContentType = "application/json"
+                };
+
+                client.PutObjectAsync(request).ConfigureAwait(false);
+            }
+        }
+    }
+}
