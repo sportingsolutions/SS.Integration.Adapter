8620356d9d28891b678a40b39d5537cf9a5634b4
sherashau <sherashau@1pt.com>
2018-02-13 15:14:04 +0300

    test

--- SS.Integration.Adapter.Tests/StreamListenerActorTests.cs
+++ SS.Integration.Adapter.Tests/StreamListenerActorTests.cs
@@ -2202,7 +2202,7 @@ namespace SS.Integration.Adapter.Tests
                     Assert.NotNull(streamListenerActorRef);
                     Assert.NotNull(streamListenerActor);
 
-                    resourceFacadeMock.Verify(a => a.GetSnapshot(), Times.Exactly(1));
+                    resourceFacadeMock.Verify(a => a.GetSnapshot(), Times.Exactly(2));
                     //resourceFacadeMock.Verify(a => a.StopStreaming(), Times.Once);
                     PluginMock.Verify(a =>
                             a.ProcessSnapshot(It.Is<Fixture>(f => f.Id.Equals(resourceFacadeMock.Object.Id)), false),
