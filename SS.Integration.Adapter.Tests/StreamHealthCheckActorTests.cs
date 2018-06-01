585b81b9e88dcca4c2a3613f06672629eff4c184
Sergey Filipov <sergey.filipov@1pt.com>
2018-06-01 18:19:46 +0300

    Fixed StreamListenerActor not found in tests

--- SS.Integration.Adapter.Tests/StreamHealthCheckActorTests.cs
+++ SS.Integration.Adapter.Tests/StreamHealthCheckActorTests.cs
@@ -92,6 +92,7 @@ namespace SS.Integration.Adapter.Tests
                 /*storedData*/new { Epoch = 3, Sequence = 1, MatchStatus = MatchStatus.Setup },
                 out snapshot,
                 out resourceFacadeMock);
+            ServiceMock.Setup(o => o.GetSports()).Returns(new[] { FootabllSportMock.Object });
             ServiceMock.Setup(o => o.GetResources(It.Is<string>(s => s.Equals(FootabllSportMock.Object.Name))))
                 .Returns(new List<IResourceFacade> { resourceFacadeMock.Object });
             StreamHealthCheckValidationMock.Setup(a =>
@@ -125,14 +126,20 @@ namespace SS.Integration.Adapter.Tests
                     Props.Create(() => new SportProcessorRouterActor(ServiceMock.Object))
                         .WithRouter(new SmallestMailboxPool(SettingsMock.Object.FixtureCreationConcurrency)),
                     SportProcessorRouterActor.ActorName);
-
+            ActorOfAsTestActorRef<SportsProcessorActor>(
+                Props.Create(() =>
+                    new SportsProcessorActor(
+                        SettingsMock.Object,
+                        ServiceMock.Object,
+                        sportProcessorRouterActor)),
+                SportsProcessorActor.ActorName);
             sportProcessorRouterActor.Tell(new ProcessSportMsg { Sport = FootabllSportMock.Object.Name });
 
             IActorRef streamListenerActorRef;
             StreamListenerActor streamListenerActor = null;
             IActorRef resourceActorRef;
             IActorRef healthCheckActorRef;
-
+           
             //Get child actors instances
             AwaitAssert(() =>
                 {
@@ -330,6 +337,7 @@ namespace SS.Integration.Adapter.Tests
                 /*storedData*/new { Epoch = 3, Sequence = 1, MatchStatus = MatchStatus.InRunning },
                 out snapshot,
                 out resourceFacadeMock);
+            ServiceMock.Setup(o => o.GetSports()).Returns(new[] { FootabllSportMock.Object });
             ServiceMock.Setup(o => o.GetResources(It.Is<string>(s => s.Equals(FootabllSportMock.Object.Name))))
                 .Returns(new List<IResourceFacade> { resourceFacadeMock.Object });
             StreamHealthCheckValidationMock.Setup(a =>
@@ -362,6 +370,13 @@ namespace SS.Integration.Adapter.Tests
                     Props.Create(() => new SportProcessorRouterActor(ServiceMock.Object))
                         .WithRouter(new SmallestMailboxPool(SettingsMock.Object.FixtureCreationConcurrency)),
                     SportProcessorRouterActor.ActorName);
+            ActorOfAsTestActorRef<SportsProcessorActor>(
+                Props.Create(() =>
+                    new SportsProcessorActor(
+                        SettingsMock.Object,
+                        ServiceMock.Object,
+                        sportProcessorRouterActor)),
+                SportsProcessorActor.ActorName);
 
             sportProcessorRouterActor.Tell(new ProcessSportMsg { Sport = FootabllSportMock.Object.Name });
 
