3d59e1fd5191f5e2241f51b5acbc9e1f9384922a
Sergey Filipov <sergey.filipov@1pt.com>
2018-06-05 14:28:09 +0300

    uncommented Once StopStreaming in test

--- SS.Integration.Adapter.Tests/StreamListenerActorTests.cs
+++ SS.Integration.Adapter.Tests/StreamListenerActorTests.cs
@@ -2205,7 +2205,7 @@ namespace SS.Integration.Adapter.Tests
                     Assert.NotNull(streamListenerActor);
 
                     resourceFacadeMock.Verify(a => a.GetSnapshot(), Times.Exactly(2));
-                    //resourceFacadeMock.Verify(a => a.StopStreaming(), Times.Once);
+                    resourceFacadeMock.Verify(a => a.StopStreaming(), Times.Once);
                     PluginMock.Verify(a =>
                             a.ProcessSnapshot(It.Is<Fixture>(f => f.Id.Equals(resourceFacadeMock.Object.Id)), false),
                         Times.Exactly(2));
