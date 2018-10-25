bbafdab512ce08d8ef05aa69ac829516a609b7cc
Evgeni <ziatikov@1pt.com>
2018-10-25 11:58:44 +0300

    set Model log4net to 2.0.3

--- SS.Integration.Adapter.Model/CachedObjectStoreWithPersistance.cs
+++ SS.Integration.Adapter.Model/CachedObjectStoreWithPersistance.cs
@@ -14,58 +14,61 @@
 
 using System;
 using SS.Integration.Adapter.Model.Interfaces;
+using log4net;
 
 namespace SS.Integration.Adapter.Model
 {
-    public class CachedObjectStoreWithPersistance<T> : CachedObjectStore<T> where T : class
-    {
-        private readonly IObjectProvider<T> _persistanceStore;
+	public class CachedObjectStoreWithPersistance<T> : CachedObjectStore<T> where T : class
+	{
+		private readonly IObjectProvider<T> _persistanceStore;
+		private readonly ILog _logger = LogManager.GetLogger(typeof(CachedObjectStoreWithPersistance<T>));
 
-        public CachedObjectStoreWithPersistance(IObjectProvider<T> persistanceStore, string cacheUniqueId,
-                                                int cacheExpiryInSecs = 60)
-            : base(cacheUniqueId, cacheExpiryInSecs)
-        {
-            _persistanceStore = persistanceStore;
-        }
+		public CachedObjectStoreWithPersistance(IObjectProvider<T> persistanceStore, string cacheUniqueId,
+												int cacheExpiryInSecs = 60)
+			: base(cacheUniqueId, cacheExpiryInSecs)
+		{
+			_persistanceStore = persistanceStore;
+		}
 
-        public override T GetObject(string id)
-        {
-            try
-            {
-                var item = base.GetObject(id);
-                var notInCache = item == null;
+		public override T GetObject(string id)
+		{
+			try
+			{
+				var item = base.GetObject(id);
+				var notInCache = item == null;
 
-                if(notInCache)
-                    item = _persistanceStore.GetObject(id);
+				if (notInCache)
+					item = _persistanceStore.GetObject(id);
 
-                if (notInCache && item != null)
-                {
-                    base.SetObject(id,item);
-                }
+				if (notInCache && item != null)
+				{
+					base.SetObject(id, item);
+				}
 
-                return item;
-            }
-            catch (Exception ex)
-            {
-                
-                Remove(id);
-                
-            }
+				return item;
+			}
+			catch (Exception ex)
+			{
+				_logger.Error(string.Format("Deserialization error occured for item with id={0}. Storage file must be corrupted and will be removed", id), ex);
 
-            return null;
-        }
+				Remove(id);
 
-        public override bool Remove(string id)
-        {
-            _persistanceStore.Remove(id);
-            base.Remove(id);
-            return true;
-        }
+			}
 
-        public override void SetObject(string id, T item)
-        {
-            base.SetObject(id, item);
-            _persistanceStore.SetObject(id, item);
-        }
-    }
+			return null;
+		}
+
+		public override bool Remove(string id)
+		{
+			_persistanceStore.Remove(id);
+			base.Remove(id);
+			return true;
+		}
+
+		public override void SetObject(string id, T item)
+		{
+			base.SetObject(id, item);
+			_persistanceStore.SetObject(id, item);
+		}
+	}
 }
--- SS.Integration.Adapter.Model/ProcessState/BinaryStoreProvider.cs
+++ SS.Integration.Adapter.Model/ProcessState/BinaryStoreProvider.cs
@@ -15,100 +15,105 @@
 using System;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using log4net;
 using SS.Integration.Adapter.Model.Interfaces;
 
 namespace SS.Integration.Adapter.Model.ProcessState
 {
-    public class BinaryStoreProvider<T> : FileStoreProvider, IObjectProvider<T>
-    {
-        private readonly string _pathFormatString;
+	public class BinaryStoreProvider<T> : FileStoreProvider, IObjectProvider<T>
+	{
+		private readonly string _pathFormatString;
+		private readonly ILog _logger = LogManager.GetLogger("SS.Integration.Adapter.Model.ProcessState.BinaryStoreProvider");
 
-        public BinaryStoreProvider(string directory, string pathFormatString)
-            : base(directory)
-        {
-            _pathFormatString = pathFormatString;
-        }
+		public BinaryStoreProvider(string directory, string pathFormatString)
+			: base(directory)
+		{
+			_pathFormatString = pathFormatString;
+		}
 
-        private string GetPath(string id)
-        {
-            return GetFullPath(string.Format(_pathFormatString,id));
-            //return string.Format(_pathFormatString, id);
-        }
+		private string GetPath(string id)
+		{
+			return GetFullPath(string.Format(_pathFormatString, id));
+			//return string.Format(_pathFormatString, id);
+		}
 
-        public T GetObject(string id)
-        {
-            var filePath = GetPath(id);
-            string serializedString;
-            try
-            {
-                serializedString = base.Read(filePath);
-            }
-            catch (Exception e)
-            {
-                DeleteFile(filePath);
-                throw;
-            }
+		public T GetObject(string id)
+		{
+			var filePath = GetPath(id);
+			string serializedString;
+			try
+			{
+				serializedString = base.Read(filePath);
+			}
+			catch (Exception e)
+			{
+				_logger.Error($"Error reading path={filePath} , file will be removed! , exception={e}");
+				DeleteFile(filePath);
+				throw;
+			}
 
-            var result = default(T);
-            if (serializedString == null)
-                return result;
+			var result = default(T);
+			if (serializedString == null)
+				return result;
 
-            var serializer = new BinaryFormatter();
+			var serializer = new BinaryFormatter();
 
-            try
-            {
-                result = (T)serializer.Deserialize(new MemoryStream(Convert.FromBase64String(serializedString)));
-            }
-            catch (Exception e)
-            {
-                DeleteFile(filePath);
-                throw;
-            }
+			try
+			{
+				result = (T)serializer.Deserialize(new MemoryStream(Convert.FromBase64String(serializedString)));
+			}
+			catch (Exception e)
+			{
+				_logger.Error($"Error converting path={filePath} , file will be removed! , exception={e}");
+				DeleteFile(filePath);
+				throw;
+			}
 
-            return result;
+			return result;
 
-        }
+		}
 
-        private void DeleteFile(string _pathFileName)
-        {
-            if (System.IO.File.Exists(_pathFileName))
-            {
-                System.IO.File.Delete(_pathFileName);
-            }
-        }
+		private void DeleteFile(string _pathFileName)
+		{
+			if (System.IO.File.Exists(_pathFileName))
+			{
+				System.IO.File.Delete(_pathFileName);
+				_logger.DebugFormat($"Deleted file {_pathFileName}");
+			}
+		}
 
-        public void SetObject(string id, T item)
-        {
-            var filePath = GetPath(id);
+		public void SetObject(string id, T item)
+		{
+			var filePath = GetPath(id);
 
-            using (var memStream = new MemoryStream())
-            {
-                var serializer = new BinaryFormatter();
-                serializer.Serialize(memStream, item);
+			using (var memStream = new MemoryStream())
+			{
+				var serializer = new BinaryFormatter();
+				serializer.Serialize(memStream, item);
 
-                var serializedString = Convert.ToBase64String(memStream.ToArray());
-                base.Write(filePath, serializedString);
-            }
+				var serializedString = Convert.ToBase64String(memStream.ToArray());
+				base.Write(filePath, serializedString);
+			}
 
-        }
+		}
 
-        public bool Remove(string id)
-        {
-            var filePath = GetPath(id);
-            if(File.Exists(filePath))
-                File.Delete(filePath);
-            return true;
-        }
+		public bool Remove(string id)
+		{
+			var filePath = GetPath(id);
+			if (File.Exists(filePath))
+				File.Delete(filePath);
+			return true;
+		}
 
-        public void Clear(string id = null)
-        {
-            if (!string.IsNullOrEmpty(id))
-                Remove(id);
-        }
+		public void Clear(string id = null)
+		{
+			if (!string.IsNullOrEmpty(id))
+				Remove(id);
+		}
 
-        public void Flush()
-        {
+		public void Flush()
+		{
 
-        }
-    }
+		}
+	}
 }
