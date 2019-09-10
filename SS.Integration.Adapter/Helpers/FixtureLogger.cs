2ff1bf103a2178a34afce20576e6ab782f8e9882
Sergey Filipov <sergey.filipov@1pt.com>
2019-09-10 12:18:37 +0300

    Fixture Logger hepler added

---
+++ SS.Integration.Adapter/Helpers/FixtureLogger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using SS.Integration.Adapter.Model;
+using SS.Integration.Adapter;
+
+namespace SS.Integration.Adapter.Helpers
+{
+    public static class FixtureLogger
+    {
+        private static string basePath = @"C:\Logs\Fixtures\";
+
+        public static void Log(Fixture fixture)
+        {
+            var path = GetPath(fixture);
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+
+            var sequence = fixture.Sequence;
+            var filename = sequence + ".json";
+            var fullFilename = Path.Combine(path, filename);
+
+            if (!File.Exists(fullFilename))
+            {
+                var json = FixtureHelper.ToJson(fixture);
+                System.IO.File.WriteAllText(fullFilename, json);
+            }
+        }
+        
+        private static string GetPath(Fixture fixture)
+        {
+            var today = DateTime.Today.ToShortDateString();
+            return Path.Combine(basePath, today, fixture.Id);
+        }
+    }
+}
